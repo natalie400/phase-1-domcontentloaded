f55c9938ce29b44155f050cde1bd7e7139fab55a
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-12 17:44:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    
+  let doc = document.getElementById("text");
+
+  doc.textContent = 'This is really cool!'
+  });
+  
+  
+
+  
